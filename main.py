71aa884f7e3b8abf38e925d97b78de7cc00fd57a
Oliver <ozhang2305@gmail.com>
2023-06-19 18:29:54 -0700

    Finishing the assignment

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     ##############################
     # make your code below
     # print('Hello World')
-    ##############################
+    ##############################\
+    print("Hello, World")
+    print("Python Programming")
 
 
 if __name__ == '__main__':
